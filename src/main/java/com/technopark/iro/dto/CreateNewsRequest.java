248a60ba1297809574da2810ab77e9bbda3dfc27
cakeslayer <mainacc1838@gmail.com>
2025-11-25 18:09:19 +0500

    chore: add todo

--- src/main/java/com/technopark/iro/dto/CreateNewsRequest.java
+++ src/main/java/com/technopark/iro/dto/CreateNewsRequest.java
@@ -4,6 +4,7 @@ import com.technopark.iro.model.NewsStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+//TODO: apply same refactoring for news domain
 public record CreateNewsRequest(@NotBlank(message = "Title cannot be empty")
                                    @Size(max = 255, message = "Title cannot exceed 255 characters")
                                    String title,
